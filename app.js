00ad87eb8e59770b2cf8a38bc0cb226404589963
nathanielc <nathanielcastilleja@yahoo.com>
2022-07-25 20:51:12 -0500

    add play functions for computer and player and make player function parameter case-insensitive

--- app.js
+++ app.js
@@ -8,7 +8,52 @@ function getComputerChoice(){
         return 'scissors';
     }
 }
-console.log(getComputerChoice());
+
+function playRound 
+(playerelection, 
+    computerSelection) {
+    if (playerSelection.
+         toLowerCase() === 'rock'){
+        if (computerSelection === 'rock'){
+            return 'Tie';
+        } else if(computerSelection === 
+                'paper'){
+                computerScore++;
+                return 'Computer Wins';
+            } else {
+                playerScore++;
+                return 'Player Wins';
+            }
+        }
+        if (playerSelection === 'paper') {
+            if (computerSelection === 'rock') {
+                playerScore++;
+                return 'Player Wins';
+            } else if (computerSelection === 'scissors') {
+                computerScore++;
+                return 'Computer Wins';
+            } else {
+                return 'Tie';
+            }
+        }
+        if (playerSelection === 'scissors') {
+            if (computerSelection === 'rock') {
+                computerScore++;
+                return 'Computer Wins';
+            } else if (computerSelection === 'paper') {
+                playerScore++;
+                return 'Player Wins';
+            } else {
+                    return 'Tie';
+                }
+            }
+        }
+
+        
+    
+
+
+
 
 
     
